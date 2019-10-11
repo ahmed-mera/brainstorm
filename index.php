d47f713b940a3a195833d88316d124dc0f70d27b
Ahmed Mera <ahmedmera81136@gmail.com>
2019-10-11 21:27:17 +0200

    brain storm v.01

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title> </title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/css/mdb.min.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body class="elegant-color-dark">
+  <!-- Start your project here-->
+  <header>
+    <!--Navbar -->
+    <nav class="mb-1 navbar navbar-expand-lg navbar-dark secondary-color lighten-1 elegant-color-dark">
+        <a class="navbar-brand" href="#">
+          <img src="./images/brainstorm.jpeg" class="rounded-circle z-depth-0 avatar mr-1" alt="brain storm image">
+          BrianStorm 
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
+          aria-controls="navbarSupportedContent-555" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-555">
+          <ul class="navbar-nav ml-auto nav-flex-icons">
+            <li class="nav-item mr-1">
+              <a class="nav-link waves-effect waves-light">
+                <i class="fas fa-search fa-lg text-white"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+                <form class="form-inline">
+                  <div class="md-form my-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder = "Search" aria-label="Search">
+                  </div>
+                </form>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link waves-effect waves-light line-height">1
+                  <i class="fas fa-envelope"></i>
+                </a>
+              </li>
+              <li class="nav-item avatar dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-55" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" class="rounded-circle z-depth-0 avatar"
+                    alt="avatar image">
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg-right dropdown-secondary"
+                  aria-labelledby="navbarDropdownMenuLink-55">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+          </ul>
+        </div>
+    </nav>
+    <!--/.Navbar -->
+    <!--Navbar-->
+    <nav class="mb-1 navbar navbar-expand-lg navbar-dark secondary-color lighten-1 elegant-color-dark d-lg-block d-none ">
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse container" id="basicExampleNav">
+
+      <!-- Links -->
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+    </nav>
+    <!--/.Navbar-->
+
+  </header>
+  <!-- Start your project here-->
+    <main>
+      <section class = "container-fluid text-white text-center pt-5 ">
+        <!-- Section: Products v.1 -->
+        <section class="text-center my-5">
+
+          <!-- Section heading -->
+          <h2 class="h1-responsive font-weight-bold text-center my-5">Our bestsellers</h2>
+          <!-- Grid row -->
+          <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-lg-3 col-md-6 mb-lg-0 mb-4 ">
+              <!-- Card -->
+              <div class="card card-cascade narrower card-ecommerce ">
+                <!-- Card image -->
+                <div class="view view-cascade overlay">
+                  <img src="./images/html.jpg" class="card-img-top"
+                    alt="sample photo">
+                  <a>
+                    <div class="mask rgba-white-slight"></div>
+                  </a>
+                </div>
+                <!-- Card image -->
+                <!-- Card content -->
+                <div class="card-body card-body-cascade text-center">
+                  <!-- Category & Title -->
+                  <a href="" class="grey-text">
+                    <h5>HTML</h5>
+                  </a>
+                  <h4 class="card-title">
+                    <strong>
+                      <a href="">HTML V.01<a>
+                    </strong>
+                  </h4>
+                  <!-- Description -->
+                  <p class="card-text">
+                  the description.........
+                  </p>
+                  <!-- Card footer -->
+                  <div class="card-footer px-1">
+                  <button class="btn blue-gradient">view</button>
+                  <button class="btn blue-gradient">like</button>
+                  </div>
+                </div>
+                <!-- Card content -->
+              </div>
+              <!-- Card -->
+            </div>
+            <!-- Grid column -->
+
+            
+            <!-- Grid column -->
+            <div class="col-lg-3 col-md-6 mb-lg-0 mb-4 ">
+              <!-- Card -->
+              <div class="card card-cascade narrower card-ecommerce ">
+                <!-- Card image -->
+                <div class="view view-cascade overlay">
+                  <img src="./images/html.jpg" class="card-img-top"
+                    alt="sample photo">
+                  <a>
+                    <div class="mask rgba-white-slight"></div>
+                  </a>
+                </div>
+                <!-- Card image -->
+                <!-- Card content -->
+                <div class="card-body card-body-cascade text-center">
+                  <!-- Category & Title -->
+                  <a href="" class="grey-text">
+                    <h5>HTML</h5>
+                  </a>
+                  <h4 class="card-title">
+                    <strong>
+                      <a href="">HTML V.01<a>
+                    </strong>
+                  </h4>
+                  <!-- Description -->
+                  <p class="card-text">
+                  the description.........
+                  </p>
+                  <!-- Card footer -->
+                  <div class="card-footer px-1">
+                  <button class="btn blue-gradient">view</button>
+                  <button class="btn blue-gradient">like</button>
+                  </div>
+                </div>
+                <!-- Card content -->
+              </div>
+              <!-- Card -->
+            </div>
+            <!-- Grid column -->
+
+            
+            <!-- Grid column -->
+            <div class="col-lg-3 col-md-6 mb-lg-0 mb-4 ">
+              <!-- Card -->
+              <div class="card card-cascade narrower card-ecommerce ">
+                <!-- Card image -->
+                <div class="view view-cascade overlay">
+                  <img src="./images/html.jpg" class="card-img-top"
+                    alt="sample photo">
+                  <a>
+                    <div class="mask rgba-white-slight"></div>
+                  </a>
+                </div>
+                <!-- Card image -->
+                <!-- Card content -->
+                <div class="card-body card-body-cascade text-center">
+                  <!-- Category & Title -->
+                  <a href="" class="grey-text">
+                    <h5>HTML</h5>
+                  </a>
+                  <h4 class="card-title">
+                    <strong>
+                      <a href="">HTML V.01<a>
+                    </strong>
+                  </h4>
+                  <!-- Description -->
+                  <p class="card-text">
+                  the description.........
+                  </p>
+                  <!-- Card footer -->
+                  <div class="card-footer px-1">
+                  <button class="btn blue-gradient">view</button>
+                  <button class="btn blue-gradient">like</button>
+                  </div>
+                </div>
+                <!-- Card content -->
+              </div>
+              <!-- Card -->
+            </div>
+            <!-- Grid column -->
+
+            
+            <!-- Grid column -->
+            <div class="col-lg-3 col-md-6 mb-lg-0 mb-4 ">
+              <!-- Card -->
+              <div class="card card-cascade narrower card-ecommerce ">
+                <!-- Card image -->
+                <div class="view view-cascade overlay">
+                  <img src="./images/html.jpg" class="card-img-top"
+                    alt="sample photo">
+                  <a>
+                    <div class="mask rgba-white-slight"></div>
+                  </a>
+                </div>
+                <!-- Card image -->
+                <!-- Card content -->
+                <div class="card-body card-body-cascade text-center">
+                  <!-- Category & Title -->
+                  <a href="" class="grey-text">
+                    <h5>HTML</h5>
+                  </a>
+                  <h4 class="card-title">
+                    <strong>
+                      <a href="">HTML V.01<a>
+                    </strong>
+                  </h4>
+                  <!-- Description -->
+                  <p class="card-text">
+                  the description.........
+                  </p>
+                  <!-- Card footer -->
+                  <div class="card-footer px-1">
+                  <button class="btn blue-gradient">view</button>
+                  <button class="btn blue-gradient">like</button>
+                  </div>
+                </div>
+                <!-- Card content -->
+              </div>
+              <!-- Card -->
+            </div>
+            <!-- Grid column -->
+          </section>
+        <!-- Section: Products v.1 -->
+    
+      </section>
+    </main>
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/js/mdb.min.js"></script>
+  <!-- sweetalert2 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
+  <script src="./js/main.js"></script>
+</body>
+</html>
